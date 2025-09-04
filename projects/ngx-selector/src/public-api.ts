e5f905482e891c07f0a5b4674a7bcb1b27f94de6
Victor <victor1203rmmtz@gmail.com>
2025-09-04 00:54:16 -0600

    selector 3

--- projects/ngx-selector/src/public-api.ts
+++ projects/ngx-selector/src/public-api.ts
@@ -4,5 +4,5 @@
 
 export * from './lib/ngx-selector.service';
 export * from './lib/ngx-selector.component';
-export * from './ui-selector/ui-selector.module';
+
 export * from './ui-selector/selector/selector.component';
